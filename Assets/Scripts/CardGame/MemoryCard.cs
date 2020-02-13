49aa51e3e61c38c4e7b32764e8c6e39b9b7b9883
Alejandro Gómez <algono@inf.upv.es>
2020-02-13 19:39:36 +0100

    Added Card Game scene

---
+++ Assets/Scripts/CardGame/MemoryCard.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject cardBack;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        cardBack.SetActive(!cardBack.activeSelf);
+    }
+}
